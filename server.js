5b37643990270878e9b2e9e177a901c9a91da7fc
Chaimae Belayachi <34791753+chaimaeBel@users.noreply.github.com>
2018-08-08 22:42:28 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+    path = require('path'),
+    nodeMailer = require('nodemailer'),
+    bodyParser = require('body-parser');
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    var app = express();
+    app.set('view engine', 'ejs');
+    app.use(express.static('public'));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    var port = 3000;
+    app.get('/', function (req, res) {
+      res.render('index');
+    });
+    app.post('/send-email', function (req, res) {
+      let transporter = nodeMailer.createTransport({
+          host: 'smtp.gmail.com',
+          auth: {
+              user: 'xxx@gmail.com',
+              pass: 'xxxx'
+          }
+      });
+      let mailOptions = {
+          from: '"belayachi chaimae" <belayachichaimae@gmail.com>', // sender address
+          to: req.body.to, // list of receivers
+          subject: req.body.subject, // Subject line
+          text: req.body.body, // plain text body
+          html: '<b>NodeJS Email Tutorial</b>' // html body
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              return console.log(error);
+          }
+          console.log('Message %s sent: %s', info.messageId, info.response);
+              res.render('index');
+          });
+      });
+          app.listen(port, function(){
+            console.log('Server is running at port: ',port);
+          });
